d4f291affd664151eb016e2b2336e6ab39f4e560
sbm <g.sakurai123@gmail.com>
2025-10-15 01:37:00 +0900

    init

---
+++ src/graphics.ts
@@ -0,0 +1,66 @@
+﻿import p5 from 'p5';
+import {Camera} from "@mediapipe/camera_utils";
+import {Segmentator} from "./inference.ts";
+
+const segmentator = new Segmentator()
+
+const videoElem = document.createElement('video') as HTMLVideoElement
+const camera = new Camera(videoElem, {
+    onFrame: async () => {
+        await segmentator.send(videoElem)
+    },
+    width: 1280,
+    height: 720
+});
+camera.start()
+
+const sketch = (p: p5) => {
+    let latestMask: HTMLImageElement | null = null;
+    segmentator.onUpdate((img) => {
+        latestMask = img;
+    });
+
+    let resultGraphics: p5.Graphics
+    const initGraphics =
+        (img: HTMLImageElement): p5.Graphics => p.createGraphics(img.width, img.height)
+
+    const res = 30
+
+    p.setup = () => {
+        const cnv = p.createCanvas(1280, 720)
+        const app = document.getElementById('app')
+        console.log(app)
+        if (!app) throw new Error('No app element found')
+        cnv.parent(app)
+    }
+
+    p.draw = () => {
+        p.clear();
+
+        const ux = p.width / res;
+        const uy = p.height / res;
+        for (let i = 0; i <= res; i++) {
+            for (let j = 0; j <= res; j++) {
+                p.fill((i + j) % 2 == 0 ? 255 : 100);
+                p.rect(i * ux, j * uy, ux, uy);
+            }
+        }
+
+        if (latestMask) {
+            if (!resultGraphics){
+                resultGraphics = initGraphics(latestMask);
+            }
+
+            resultGraphics.drawingContext.clearRect(0, 0, resultGraphics.width, resultGraphics.height);
+            resultGraphics.drawingContext.drawImage(latestMask, 0, 0);
+
+            p.image(resultGraphics, 0, 0, p.width, p.height);
+        }
+    }
+
+    p.keyPressed = () => {
+        p.fullscreen(true)
+    }
+}
+
+new p5(sketch)
---
+++ src/inference.ts
@@ -0,0 +1,43 @@
+﻿import {SelfieSegmentation} from "@mediapipe/selfie_segmentation";
+import type {Results} from "@mediapipe/selfie_segmentation";
+
+export class Segmentator {
+    private model: SelfieSegmentation;
+    private listeners: ((img: HTMLImageElement) => void)[] = [];
+
+    constructor() {
+        this.model = new SelfieSegmentation({
+            locateFile: (file) => {
+                return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
+            }
+        });
+        this.model.setOptions({
+            modelSelection: 1,
+        });
+        this.model.onResults(this.onResults);
+    }
+
+    private onResults = (results: Results) => {
+        if (!results.segmentationMask) return;
+        const canvas = document.createElement('canvas');
+        canvas.width = results.segmentationMask.width;
+        canvas.height = results.segmentationMask.height;
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+
+        ctx.drawImage(results.segmentationMask, 0, 0);
+        const img = new Image();
+        img.src = canvas.toDataURL();
+
+        // リスナーを呼び出す
+        this.listeners.forEach(fn => fn(img));
+    };
+
+    public onUpdate(fn: (img: HTMLImageElement) => void) {
+        this.listeners.push(fn);
+    }
+
+    public async send(input: HTMLVideoElement | HTMLImageElement | HTMLCanvasElement) {
+        await this.model.send({image: input});
+    }
+}
