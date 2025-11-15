46b288c154100a6b020303c77841a90fcd1a7c9c
sbm <g.sakurai123@gmail.com>
2025-11-15 20:27:04 +0900

    more goooood palettes
    shoutout to coolors.co

--- src/palette.ts
+++ src/palette.ts
@@ -4,8 +4,10 @@ export type FixedLengthArray<T, N extends number, A extends T[] = []> =
 export const PLT_LEN = 5
 
 export const palettes: FixedLengthArray<string, 5>[] = [
-    ["#220c10","#506c64","#77cbb9","#75b8c8","#cdd3d5"],
+    ["#d7263d","#f46036","#2e294e","#1b998b","#c5d86d"],
+    ["#2d3142","#bfc0c0","#ffffff","#ef8354","#4f5d75"],
+    ["#386641","#6a994e","#a7c957","#f2e8cf","#bc4749"],
     ["#eac435","#345995","#e40066","#03cea4","#fb4d3d"],
-    ["#230903","#656256","#9ebc9f","#d3b88c","#f4f2f3"],
+    ["#e63946","#f1faee","#a8dadc","#457b9d","#1d3557"],
     ["#dad2d8","#143642","#0f8b8d","#ec9a29","#a8201a"]
 ]
